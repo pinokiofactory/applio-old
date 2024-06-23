89e874b0ee8906450858920ae8d11bf3ff405425
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-23 15:40:43 -0400

    update

--- install.js
+++ install.js
@@ -8,46 +8,58 @@ module.exports = {
         ]
       }
     },
-    {
-      method: "fs.download",
-      params: {
-        uri: "https://huggingface.co/IAHispano/Applio/resolve/main/env.zip",
-        dir: "applio"
-      }
-    },
+//    {
+//      method: "fs.download",
+//      params: {
+//        uri: "https://huggingface.co/IAHispano/Applio/resolve/main/env.zip",
+//        dir: "applio"
+//      }
+//    },
+//    {
+//      method: "shell.run",
+//      params: {
+//        path: "applio",
+//        message: [
+//          "unzip env.zip",
+//          "rm env.zip"
+//        ],
+//      }
+//    },
+//    {
+//      method: "shell.run",
+//      params: {
+//        path: "applio",
+//        message: [
+//          "conda create --no-shortcuts -y -k --prefix env python=3.9",
+//        ],
+//      }
+//    },
     {
       method: "shell.run",
       params: {
-        path: "applio",
-        message: [
-          "unzip env.zip",
-          "rm env.zip"
-        ],
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        path: "applio",
-        message: [
-          "conda create --no-shortcuts -y -k --prefix env python=3.9",
-        ],
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        conda: {
-          path: "env",
-          python: "python=3.9"
-        },
+        venv: "env",
+//        conda: {
+//          path: "env",
+//          python: "python=3.9"
+//        },
         path: "applio",
         message: [
           "pip install --upgrade setuptools",
           "pip install -r requirements.txt",
-          "pip uninstall torch torchvision torchaudio -y",
-          "pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121",
+          //"pip uninstall torch torchvision torchaudio -y",
+          //"pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121",
         ],
+      }
+    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "applio",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
       }
     },
     {
